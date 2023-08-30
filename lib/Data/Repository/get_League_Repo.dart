72e65a1926fd3a43030d3428e6a72e465f97843e
HussieneElmorsy1 <hussien1elmorsy@gmail.com>
2023-08-30 20:38:40 +0300

    hussien

--- lib/Data/Repository/get_League_Repo.dart
+++ lib/Data/Repository/get_League_Repo.dart
@@ -2,17 +2,12 @@ import 'dart:convert';
 import 'package:sport_app_semicolon/Data/Models/get_League_Model.dart';
 import 'package:http/http.dart' as http;
 
-
-
 class GetLeagueRepo {
   String apiKey =
       '7d48b49a91d2c3149e5d0b3e7fe68faa5aa51df9375c5d1cba5710191c9225a1';
   Future<LeagueModel?> getLeague() async {
     try {
       var response = await http.get(Uri.parse(
-
-          "https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=ba8cce3a55c7eda219fca0330f1ac53671a427aea4542ae118bd628d6b494a67"));
-
           "https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=$apiKey"));
 
       var decodedResponse = jsonDecode(response.body);
--- lib/Screens/LeagueScreen.dart
+++ lib/Screens/LeagueScreen.dart
@@ -2,17 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sport_app_semicolon/Cubits/LeagueCubit/league_cubit.dart';
-
-import 'package:sport_app_semicolon/Cubits/TeamsCubit/get_goals_cubit.dart';
-import 'package:sport_app_semicolon/Cubits/TeamsCubit/get_teams_cubit.dart';
-import 'package:sport_app_semicolon/Data/Repository/get_League_Repo.dart';
-import 'package:sport_app_semicolon/Data/Repository/get_teams_data_repo.dart';
-import 'package:sport_app_semicolon/Screens/TeamsScreen.dart';
-
-import 'package:sport_app_semicolon/screens/test.dart';
+import 'package:sport_app_semicolon/Screens/test.dart';
 
 class LeagueScreen extends StatefulWidget {
-  const LeagueScreen({super.key});
+  const LeagueScreen({super.key, required String countryName});
 
   @override
   State<LeagueScreen> createState() => _LeagueScreenState();
@@ -28,76 +21,60 @@ class _LeagueScreenState extends State<LeagueScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            "LEAGUES",
-            style: GoogleFonts.lato(color: Colors.white, fontSize: 30),
-          ),
-          backgroundColor: Colors.black,
-          automaticallyImplyLeading: false,
-        ),
         body: SafeArea(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            color: Colors.black,
-            child: BlocBuilder<LeagueCubit, LeagueState>(
-              builder: (context, state) {
-                if (state is LeagueLoading) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (state is LeagueSuccess) {
-                  return ListView.builder(
-                      itemCount: state.response?.result.length,
-                      itemBuilder: (BuildContext context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: GestureDetector(
-                            onTap: () {
-
-                              league_id =
-                                  state.response!.result[index].leagueKey;
-                              print(league_id);
-                              context.read<GetGoalsCubit>().getGoals();
-                              context.read<GetTeamsCubit>().getTeams();
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => TeamsScreen()),
-
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => Test()),
-
-                              );
-                            },
-                            child: Container(
-                              height:
-                                  MediaQuery.of(context).size.height * 1 / 3,
-                              width: MediaQuery.of(context).size.width,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                  color: Colors.white,
-                                  image: DecorationImage(
-                                      image: NetworkImage(state.response
-                                              ?.result[index].leagueLogo ??
-                                          "https://upload.wikimedia.org/wikipedia/ar/f/f7/Fifa-logo.png?20140204004927"),
-                                      fit: BoxFit.contain)),
-                            ),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.black,
+        child: BlocBuilder<LeagueCubit, LeagueState>(
+          builder: (context, state) {
+            if (state is LeagueLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is LeagueSuccess) {
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "LEAGUES",
+                      style:
+                          GoogleFonts.lato(fontSize: 40, color: Colors.white),
+                    ),
+                    for (int i = 0; i < state.response!.result.length; i++)
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Test()),
+                            );
+                          },
+                          child: Container(
+                            height: MediaQuery.of(context).size.height * 1 / 3,
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.white,
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        state.response?.result[i].leagueLogo ??
+                                            ""),
+                                    fit: BoxFit.contain)),
                           ),
-                        );
-                      });
-                  //Text(
-                  //"LEAGUES",
-                  // style:
-                  //    GoogleFonts.lato(fontSize: 40, color: Colors.white),
-                  //),
-                } else {
-                  return const Center(child: Text("Something wrong happened"));
-                }
-              },
-            ),
-          ),
-        ));
+                        ),
+                      ),
+                  ],
+                ),
+              );
+            } else {
+              return const Center(child: Text("Something wrong happened"));
+            }
+          },
+        ),
+      ),
+    ));
   }
 }
