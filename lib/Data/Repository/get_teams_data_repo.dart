62cff204cfe5eb44ed2d4f7baf0e21a5a6ca9dfd
unknown <ahmedothmanalhalwagy@gmail.com>
2023-08-30 13:35:08 +0300

    ahmed

---
+++ lib/Data/Repository/get_teams_data_repo.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:sport_app_semicolon/Data/Models/get_goals_model.dart';
+import 'package:sport_app_semicolon/Data/Models/get_teams_model.dart';
+import 'package:sport_app_semicolon/Data/Repository/get_League_Repo.dart';
+
+var league_id;
+
+class GetTeamsRepo {
+  Future<GetTeamsModel?> getTeams() async {
+    try {
+      var teamsresponse = await http.get(
+        Uri.parse(
+            "https://apiv2.allsportsapi.com/football/?&met=Teams&leagueId=${league_id}&APIkey=ba8cce3a55c7eda219fca0330f1ac53671a427aea4542ae118bd628d6b494a67"),
+      );
+
+      var decodedResponse = jsonDecode(teamsresponse.body);
+
+      if (teamsresponse.statusCode == 200) {
+        GetTeamsModel myResponse = GetTeamsModel.fromJson(decodedResponse);
+
+        return myResponse;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      return null;
+    }
+  }
+}
+
+class GetGoalsRepo {
+  Future<GetGoalsModel?> getGoals() async {
+    try {
+      var goalsresponse = await http.get(
+        Uri.parse(
+            "https://apiv2.allsportsapi.com/football/?&met=Topscorers&leagueId=${league_id}&APIkey=ba8cce3a55c7eda219fca0330f1ac53671a427aea4542ae118bd628d6b494a67"),
+      );
+
+      var decodedResponse = jsonDecode(goalsresponse.body);
+
+      if (goalsresponse.statusCode == 200) {
+        GetGoalsModel myResponse = GetGoalsModel.fromJson(decodedResponse);
+
+        return myResponse;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      return null;
+    }
+  }
+}
